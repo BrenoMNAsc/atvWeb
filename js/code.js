d2359ffe44c37cfbaaab24645b6a99368772e7f1
escumalha <breno.golo@gmail.com>
2024-01-29 14:44:29 -0300

    adição sidebar completa

--- js/code.js
+++ js/code.js
@@ -1,16 +1,49 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var navList = document.getElementById('navList');
+var navItems = document.getElementsByClassName('navItem');
+Array.from(navItems).forEach(navItem => {
+    var bt = navItem.querySelector('.dropdown-bt');
+    let dropmenu = navItem.querySelector('.underDrop');
 
-    for (let i = 0; i < 12; i++) {
-        var novoItem = document.createElement('li');
-        var novoLink = document.createElement('a');
-
-        novoLink.href = '#';
-        novoLink.className ='navLink';
-        novoItem.className = 'navItem';
-        novoLink.textContent = `Página ${i}`;
-
-        novoItem.appendChild(novoLink);
-        navList.appendChild(novoItem);
-    }
+    bt.addEventListener('click', () => {
+        const icon = bt.querySelector('.bi-arrow-down-short');
+        console.log(dropmenu)
+        if (icon.classList.contains('rotateIcon')) {
+            icon.classList.remove('rotateIcon');
+            dropmenu.style.height = '0';
+            icon.classList.add('rotateIconRev');
+        } else if (icon.classList.contains('rotateIconRev')) {
+            icon.classList.remove('rotateIconRev');
+            icon.classList.add('rotateIcon');
+            dropmenu.style.height = dropmenu.scrollHeight + 'px';
+        } else {
+            icon.classList.add('rotateIcon');
+            dropmenu.style.height = dropmenu.scrollHeight + 'px';
+        }
+    });
 });
+
+var openMobilebt = document.getElementById('openMobileButton')
+openMobilebt.addEventListener('click', () => {
+    var nav = document.getElementsByTagName('nav')
+    nav[0].classList.contains('openMobileMenu') ? nav[0].classList.remove("openMobileMenu") : nav[0].classList.add('openMobileMenu')
+})
+var searchBar = document.getElementsByClassName('searchBar')
+
+var input = searchBar[0].querySelector('#searchInputId1')
+var searchIcon = searchBar[0].querySelector('#searchIcon')
+
+var iconX = searchIcon.querySelector('.bi-x')
+input.addEventListener('focusout', () => {
+    var iconSearch = searchIcon.querySelector('.bi-search')
+    var iconX = searchIcon.querySelector('.bi-x')
+    iconX.style.display = 'none'
+    iconSearch.style.display = 'block'
+})
+input.addEventListener('focusin', () => {
+    if (window.screen.availWidth < 575) {
+        var iconX = searchIcon.querySelector('.bi-x')
+        var iconSearch = searchIcon.querySelector('.bi-search')
+        iconSearch.style.display = 'none'
+        iconX.style.display = 'block'
+    }
+
+})
